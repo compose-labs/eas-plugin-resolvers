3c0ee91e24215d76176b677fca21406f0c04bf41
kylekaplan <kylekaplan50@gmail.com>
2024-11-30 20:32:23 -0500

    formatting

--- src/libraries/Predeploys.sol
+++ src/libraries/Predeploys.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.15;
 
 /**
@@ -8,7 +9,8 @@ pragma solidity ^0.8.15;
  */
 library Predeploys {
     /// @notice Address of the SchemaRegistry predeploy.
-    address internal constant SCHEMA_REGISTRY = 0x4200000000000000000000000000000000000020;
+    address internal constant SCHEMA_REGISTRY =
+        0x4200000000000000000000000000000000000020;
 
     /// @notice Address of the EAS predeploy.
     address internal constant EAS = 0x4200000000000000000000000000000000000021;
