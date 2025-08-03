2a8858924574186e994a0af4dddaf61ec9985b33
kylekaplan <kylekaplan50@gmail.com>
2025-08-03 14:17:03 -0700

    use named import

--- script/DeployPluginResolverFactory.s.sol
+++ script/DeployPluginResolverFactory.s.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 import {Script, console2} from "forge-std/Script.sol";
-import "../src/factories/PluginResolverFactory.sol";
+import {PluginResolverFactory} from "../src/factories/PluginResolverFactory.sol";
 
 contract DeployPluginResolverFactory is Script {
     function setUp() public {}
